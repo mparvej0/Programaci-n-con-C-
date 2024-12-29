01d54056121411619d53b4a34c007daaaf444e7e
MD PARVEJ <mparvejind@gmail.com>
2024-12-29 21:28:20 +0530

    upd

---
+++ variable.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    cout << age << endl;
+    cout << sizeof(age) << endl;
+
+    char grade = 'A';
+    cout << grade << endl;
+
+    float PI = 3.14f;
+    cout << PI << endl;
+
+    bool isSafe = true;
+    cout << isSafe << endl; //true -> 1 & fale -> 0
+
+    double price = 100.99;
+    cout << price << endl;
+    return 0;
+}
