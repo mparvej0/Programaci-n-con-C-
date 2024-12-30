8ae5f929b130c3d679035a706dc4821694889667
MD PARVEJ <mparvejind@gmail.com>
2024-12-30 12:28:10 +0530

    upd

---
+++ Variable, Data Types & Operators/input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter the age : ";
+    cin >> age;
+    cout << "Your age is : " << age << endl;
+
+    double price;
+    cout << "Enter the price : ";
+    cin >> price;
+    cout << "You entered price = " << price << endl;
+
+    return 0;
+}
