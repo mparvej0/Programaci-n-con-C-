bb6405426c1b486276255886c5aa80db0e2c2990
MD PARVEJ <mparvejind@gmail.com>
2024-12-30 12:14:35 +0530

    upd

---
+++ typecasting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char grade = 'A';
+    int value = grade;
+    cout << value << endl;
+
+    double price = 10.99;
+    int newPrice = (int)price;
+    cout << newPrice << endl;
+
+    return 0;
+}
